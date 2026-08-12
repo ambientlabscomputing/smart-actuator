38c1c5b8451b53f46e8cd818d8709e038cfebb84
Jose Catarino <jfcatarino1@gmail.com>
2026-08-11 20:58:44 -0600

    working condition

--- cad/src/cad/main.py
+++ cad/src/cad/main.py
@@ -1,8 +1,10 @@
 import typer
 from cad.commands.export import export
+from cad.commands.objects import objects
 
 app = typer.Typer()
 app.add_typer(export, name="export", help="Export commands")
+app.add_typer(objects, name="objects", help="Object commands")
 
 if __name__ == "__main__":
     app()
